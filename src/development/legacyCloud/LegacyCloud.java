a9179e482266ffcadc45d7907079bbd73d110ebf
RandomHashTags <ImRandomHashTags@gmail.com>
2017-02-24 13:43:42 -0600

    a

--- src/development/legacyCloud/LegacyCloud.java
+++ src/development/legacyCloud/LegacyCloud.java
@@ -4,7 +4,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import development.legacyCloud.randomHashTags.yoyo.randomHashTags.playerDeathEvents;
+import development.legacyCloud.randomHashTags.yoyo.events.playerDeathEvents;
 
 public class LegacyCloud extends JavaPlugin {
 	public Plugin getPlugin;
