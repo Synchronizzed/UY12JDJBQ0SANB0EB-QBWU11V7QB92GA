55795fd0278029aef41be1323edd2f1a3c7c58b4
johnkz147 <johnkz147@gmail.com>
2017-03-05 18:26:41 -0500

    CoreCmd
    Added module CoreCmd.

---
+++ LegacyCloud/src/core/components/corecmd/CoreCmdLoader.java
@@ -0,0 +1,45 @@
+package core.components.corecmd;
+
+import java.util.ArrayList;
+
+import com.google.common.collect.Lists;
+
+import core.components.Component;
+import net.md_5.bungee.api.ChatColor;
+
+public class CoreCmdLoader extends Component{
+
+    ArrayList<String> list;
+	static CoreCmdLoader cmd;
+	
+	@Override
+	public void onEnable() {
+
+		list = Lists.newArrayList();
+		assembleList();
+		new CoreCmd("legacycore", 0, 1, "Get information about the LegacyCore core.");
+		
+	}
+
+	private void assembleList() {
+		
+		list.add(ChatColor.GREEN + "LegacyCore - V.1");
+		list.add(ChatColor.GREEN +"Authors...");
+		list.add(ChatColor.GREEN + "Yoyo");
+		list.add(ChatColor.GREEN + "JohnKZ");
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	    cmd = null;
+		
+	}
+	
+	public  ArrayList<String> getList() { return list; }
+
+	public static CoreCmdLoader getCmd() {return cmd; }
+	
+
+}
